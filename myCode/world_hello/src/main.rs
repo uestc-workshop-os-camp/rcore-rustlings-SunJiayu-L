165023e3e7ecbb1f7431c9e61949b1d1bf4e51fe
SunRose-L <630683723@qq.com>
2024-09-21 17:37:08 +0800

    second update

--- myCode/world_hello/src/main.rs
+++ myCode/world_hello/src/main.rs
@@ -1,18 +1,6 @@
-#[derive(Debug)]
- struct File {
-   name: String,
-   data: Vec<u8>,
- }
+fn main(){
+  let arr = [1, 2, 3];
+  println!("{}", arr);
+  println!("{:?}", arr);
 
- fn main() {
-   let f1 = File {
-     name: String::from("f1.txt"),
-     data: Vec::new(),
-   };
-
-   let f1_name = &f1.name;
-   let f1_length = &f1.data.len();
-
-   println!("{:?}", f1);
-   println!("{} is {} bytes long", f1_name, f1_length);
- }
+}
